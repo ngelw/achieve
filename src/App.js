0a9a227272359f7095f8501acb8ee7f2ba62ba5d
Angel Singh Shrestha <angelshrestha2024@gmail.com>
2025-05-13 00:48:18 -0500

    fix 1

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <Navbar />
+      <HomePage />
       <main>
       <Routes>
         <Route path="/lovecount" element={<LoveCount />} />
